590ff428694e6eca9b5d564811309eea411b5c42
Rasbats <miker@members.swis.net>
2023-11-30 16:19:23 +0000

    Update otcurrentOverlayFactory.h

--- src/otcurrentOverlayFactory.h
+++ src/otcurrentOverlayFactory.h
@@ -36,9 +36,7 @@ class piDC;
 class wxDC;
 
 #if defined(__ANDROID__) || defined(__OCPN__ANDROID__)
-#include "GL/gl_private.h"
 #include <qopengl.h>
-typedef double GLdouble;
 #elif defined(__APPLE__)
 #include "OpenGL/gl.h"
 #include "OpenGL/glu.h"
